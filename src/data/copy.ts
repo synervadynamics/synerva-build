5437fc65ae9d1134a32db346a278b3d9a2585b9e
Kyle Gruarin <synerva.dynamics@gmail.com>
2025-12-27 21:52:01 -0500

    hjh

--- src/data/copy.ts
+++ src/data/copy.ts
@@ -324,7 +324,7 @@ export const copy = {
           "Clear visibility into ownership + flow",
         ],
         href: "/synerva-os",
-        image: "/systems-homepage-logos/synerva-os-homepage.png",
+        image: "/systems-homepage-logos/synerva-os-homepage-logo.png",
         imageAlt: "Synerva OS logo",
       },
     ],
