3b8aaa3925ad5b6296f60ff4cb5a310c64b65fda
Kyle Gruarin <synerva.dynamics@gmail.com>
2026-01-22 00:07:49 -0500

    kk

--- src/app/backuphomepage3/BackupHomepage3.tsx
+++ src/app/backuphomepage3/BackupHomepage3.tsx
@@ -18,7 +18,7 @@ import SubpageStaticBackground from "@/components/SubpageStaticBackground";
 export default function BackupHomepage3() {
   return (
     <main className="relative text-white backuphomepage backuphomepage-variant">
-      <SubpageStaticBackground imageUrl="/subpage-backgrounds/ChatGPT Image Jan 22, 2026, 12_00_43 AM.png" />
+      <SubpageStaticBackground imageUrl="/subpage-backgrounds/ChatGPT%20Image%20Jan%2022,%202026,%2012_00_43%20AM.png" />
       <div className="pointer-events-none fixed inset-0 z-[5] bg-black/80" />
       <div className="relative z-10">
         <ScrollProgress />
--- src/app/backuphomepage3/BackupHomepage3Mobile.tsx
+++ src/app/backuphomepage3/BackupHomepage3Mobile.tsx
@@ -7,7 +7,7 @@ import styles from "@/app/mobile1/mobile1.module.css";
 export default function BackupHomepage3Mobile() {
   return (
     <main className="text-white">
-      <Mobile1Shell backgroundImageUrl="/subpage-backgrounds/ChatGPT Image Jan 22, 2026, 12_00_43 AM.png">
+      <Mobile1Shell backgroundImageUrl="/subpage-backgrounds/ChatGPT%20Image%20Jan%2022,%202026,%2012_00_43%20AM.png">
         <section
           className={`mt-6 flex flex-col gap-5 rounded-[2rem] border border-white/20 bg-white/[0.04] px-5 py-6 ${styles.panelTransparent}`}
         >
